260aefdede8a3db9de80639f315907583f5b5767
TedXpro <johannes.woldeyes@gmail.com>
2023-03-22 11:32:01 +0300

    Rank students based on average score

--- Project/struct.cpp
+++ Project/struct.cpp
@@ -9,7 +9,8 @@ struct StudentScore
     double averageScore;
 };
 
-void selectionSort(StudentScore &, int);
+void accept(StudentScore[], int);
+void sort(StudentScore [], int);
 void displayData(StudentScore [], int);
 
 int main()
@@ -20,45 +21,35 @@ int main()
 
     StudentScore student[totStudents];
 
-    for (int i = 0; i < totStudents; i++)
+    accept(student, totStudents);
+    sort(student, totStudents);
+    displayData(student, totStudents);
+    return 0;
+}
+
+/**
+ * This function will accept students information.
+ * 
+ * @param stu holds the students information.
+ * @param size holds the number of students.
+ */
+void accept(StudentScore stu[], int size)
+{
+    for (int i = 0; i < size; i++)
     {
         cout << "Enter the first name of student " << (i + 1) << ": ";
-        cin >> student[i].firstName;
-        cout << "Enter the last name of " << student[i].firstName << ": ";
-        cin >> student[i].lastName;
-        cout << "Enter the Average Score of " << student[i].firstName << ": ";
-        cin >> student[i].averageScore;
-        while(student[i].averageScore > 100 || student[i].averageScore < 0)
+        cin >> stu[i].firstName;
+        cout << "Enter the last name of " << stu[i].firstName << ": ";
+        cin >> stu[i].lastName;
+        cout << "Enter the Average Score of " << stu[i].firstName << ": ";
+        cin >> stu[i].averageScore;
+        while (stu[i].averageScore > 100 || stu[i].averageScore < 0)
         {
             cout << "The average Score of the students must be between 0 and 100.\n";
-            cout << "Enter the Average Score of " << student[i].averageScore << ": ";
-            cin >> student[i].averageScore;
+            cout << "Enter the Average Score of " << stu[i].averageScore << ": ";
+            cin >> stu[i].averageScore;
         }
     }
-
-    // selectionSort(student, totStudents);
-
-    int maxValue, maxIndex, i;
-    StudentScore temp;
-
-    for (i = 0; i < (totStudents - 1); i++)
-    {
-        maxValue = student[i].averageScore;
-        maxIndex = i;
-        temp = student[i];
-        for (int index = i + 1; index < totStudents; index++)
-        {
-            if (student[index].averageScore > maxValue)
-            {
-                maxValue = student[index].averageScore;
-                maxIndex = index;
-                temp = student[maxIndex];
-            }
-        }
-        student[maxIndex] = student[i];
-        student[i] = temp;
-    }
-    displayData(student, totStudents);
 }
 
 /**
@@ -70,29 +61,38 @@ void displayData(StudentScore stu[], int totStu)
 {
     for (int i = 0; i < totStu; i++)
     {
-        cout << "Name: " << stu[i].firstName << " " << stu[i].lastName << endl;
+        cout << "\nName: " << stu[i].firstName << " " << stu[i].lastName << endl;
         cout << "Score: " << stu[i].averageScore << endl;
         cout << "Rank: " << (i + 1) << endl;
     }
 }
 
 /**
- * This function will sort the students based on highest mark.
+ * This function will sort the students based on highest mark
+ * using seletion sort.
  *
  * @param students holds the students.
  * @param size holds the size of the array.
  */
-// void selectionSort(StudentScore &students[], int size)
-// {
-    
-// }
-
-// int no;
-// StudentScore student[no];
-// student[2] milka
-//     student[0] yohannes
-//         student[1] menilik
-// studenScore temp
-// temp = student[2]
-// student [2] = student[1]
-// student[1] = temp
+void sort(StudentScore students[], int size)
+{
+    int maxValue, maxIndex, i;
+    StudentScore temp;
+    for (i = 0; i < (size - 1); i++)
+    {
+        maxValue = students[i].averageScore;
+        maxIndex = i;
+        temp = students[i];
+        for (int index = i + 1; index < size; index++)
+        {
+            if (students[index].averageScore > maxValue)
+            {
+                maxValue = students[index].averageScore;
+                maxIndex = index;
+                temp = students[maxIndex];
+            }
+        }
+        students[maxIndex] = students[i];
+        students[i] = temp;
+    }
+}
